96f4fd2108901ee8b16af2c898912e3f77651ecc
Devin Bailie <bailied2@winthrop.edu>
2025-12-18 12:45:18 -0500

    fixed comment at top of file

--- mri-tumor-detection.py
+++ mri-tumor-detection.py
@@ -2,8 +2,8 @@
 #  Student Name: Devin Bailie
 #  Student ID: bailied2 / W30644462
 #  Course Code: CSCI 460 -- Fall 2025
-#  Assignment Due Date: 10/14/2025
-#  GitHub Link: https://github.com/bailied2/csci460-naive-bayes
+#  Assignment Due Date: 12/9/2025
+#  GitHub Link: https://github.com/bailied2/csci460-mri-tumor-detection
 ######################################################
 
 
